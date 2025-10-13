6e7dbd68402629315ab46a6adde9602bcb0042c8
josephcarrothers863-collab <josephcarrothers863@gmail.com>
2025-10-13 14:47:07 +0100

    changed the path to the database

--- Database test.py
+++ Database test.py
@@ -1,11 +1,11 @@
 
 import csv 
 
-with open('C:/Users/Josep/OneDrive/Desktop/Sixthform/2D array work/dice.csv', 'w') as file: # This creates the new file dumbass
+with open('databases/dice.csv', 'w') as file: # This creates the new file dumbass
     writer = csv.writer(file)
     writer.writerow(["player 1", "Player 2"])
 
-with open("C:/Users/Josep/OneDrive/Desktop/Sixthform/2D array work/dice.csv", "r") as file: # This reads the csv file to print the row for testing purposes since I am the goat
+with open("databases/dice.csv", "r") as file: # This reads the csv file to print the row for testing purposes since I am the goat
     content = csv.reader(open("dice.csv"))
     header = next(content)
 
