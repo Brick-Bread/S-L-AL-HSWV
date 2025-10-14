2a4e268de558d8a60c35ed167a4c705f9ab58169
josephcarrothers863-collab <josephcarrothers863@gmail.com>
2025-10-14 12:29:25 +0100

    Update snakesandladders.py

--- PYTHON FILES/snakesandladders.py
+++ PYTHON FILES/snakesandladders.py
@@ -8,12 +8,12 @@ salboard=[["43", "44", "45", "46", "47", "48", "49"],
 
 import random
 def s_lad():
-  player_1total = 0 #equivilent to position
-  player_2total = 0 #equivilent to position
-  prompt1 = 0 #used to prompt user to roll dice
-  prompt2 = 0 #used to prompt user to roll dice
-  round = 0 #used to count rounds
-  #intro to game and rules
+  player_1total = 0 # equivilent to position
+  player_2total = 0 # equivilent to position
+  prompt1 = 0 # used to prompt user to roll dice
+  prompt2 = 0 # used to prompt user to roll dice
+  round = 0 # used to count rounds
+  # intro to game and rules
   print("Welcome to Snakes and Ladders")
   print("===================================")
   print("here are the rules:")
@@ -29,7 +29,7 @@ def s_lad():
   print("====================================")
   print("*IMPORTANT* First player to reach 49 wins:)")
   
-  #main game loop which continues until one player reaches 49
+  # main game loop which continues until one player reaches 49
   
   ladder1=random.randint(8,49)
   ladder2=random.randint(8,49)
@@ -41,28 +41,28 @@ def s_lad():
   snake4=random.randint(1,35)
 
   while player_1total < 49 and player_2total < 49:
-    round += 1 #counts rounds
-    print(f"Round {round}") #shows round number
+    round += 1 # counts rounds
+    print(f"Round {round}") # shows round number
     
-    prompt1 = input("Player 1, press enter to roll the dice") #prompts player 1 to  iniciate the rolling of both dices
-    roll1 = random.randint(1,6) #rolls dice 1
-    roll2 = random.randint(1,6) #rolls dice 2
+    prompt1 = input("Player 1, press enter to roll the dice") # prompts player 1 to  iniciate the rolling of both dices
+    roll1 = random.randint(1,6) # rolls dice 1
+    roll2 = random.randint(1,6) # rolls dice 2
 
-    if roll1 == roll2: #checks if a double was rolled
-      print(f"Double was rolled, you go back {roll1 + roll2} spaces") #informs player they rolled a double
-      player_1total -= roll1 + roll2 #moves player back the amount rolled (look at the task we were given and it says we need to do this)
-    print(f"Player 1 rolled a {roll1} and a {roll2}") #informs player of what they rolled
-    player_1total += roll1 + roll2 #moves player forward the amount rolled
+    if roll1 == roll2: # checks if a double was rolled
+      print(f"Double was rolled, you go back {roll1 + roll2} spaces") # informs player they rolled a double
+      player_1total -= roll1 + roll2 # moves player back the amount rolled (look at the task we were given and it says we need to do this)
+    print(f"Player 1 rolled a {roll1} and a {roll2}") # informs player of what they rolled
+    player_1total += roll1 + roll2 # moves player forward the amount rolled
 
-    if player_1total < 1: #checks if player position is below 1 (a failsafe from an error i encountered while testing)
-      player_1total = 1 #sets player position to 1 if below 1 cah board starts from 1 to 49
+    if player_1total < 1: # checks if player position is below 1 (a failsafe from an error i encountered while testing)
+      player_1total = 1 # sets player position to 1 if below 1 cah board starts from 1 to 49
 
-    if player_1total > 49: #checks if player position is above 49
-      player_1total = 49  #sets player position to 49 if above 49 as the board ends at 49 (again i encountered an error where when i had this a player would roll above 49 and it would break the game not display the win message and just end)
-    print(f"player 1 is moving {roll1 + roll2} spaces") #informs player how many spaces they are moving
+    if player_1total > 49: # checks if player position is above 49
+      player_1total = 49  # sets player position to 49 if above 49 as the board ends at 49 (again i encountered an error where when i had this a player would roll above 49 and it would break the game not display the win message and just end)
+    print(f"player 1 is moving {roll1 + roll2} spaces") # informs player how many spaces they are moving
 
-    if player_1total == 3: #checks if player landed on a ladder
-      player_1total = 22 #moves player to the top of the ladder
+    if player_1total == 3: # checks if player landed on a ladder
+      player_1total = 22 # moves player to the top of the ladder
       print("Player 1 climbed a ladder to ",ladder1)
 
     elif player_1total == 5:
@@ -93,17 +93,17 @@ def s_lad():
       player_1total = 3
       print("Player 1 got bitten by a snake to ",snake4) 
 
-    if player_1total == 49: #checks if player 1 has won
-      print("Player 1 wins!") #informs players that player 1 has won
-      print(f"player 2 was on {player_2total}") #informs players of player 2 position
-      print("Game Over") #informs players that the game is over
-      break #breaks the loop to end the game (defo didnt have to revise where to put breaks ......)
+    if player_1total == 49: # checks if player 1 has won
+      print("Player 1 wins!") # informs players that player 1 has won
+      print(f"player 2 was on {player_2total}") # informs players of player 2 position
+      print("Game Over") # informs players that the game is over
+      break # breaks the loop to end the game (defo didnt have to revise where to put breaks ......)
     
     #player 2 turn (same as player 1 but for player 2) all in same round
     
-    prompt2 = input("Player 2, press enter to roll the dice") #prompts player 2 to  iniciate the rolling of both dices
-    roll1 = random.randint(1,6) #rolls dice 1
-    roll2 = random.randint(1,6) #rolls dice 2
+    prompt2 = input("Player 2, press enter to roll the dice") # prompts player 2 to  iniciate the rolling of both dices
+    roll1 = random.randint(1,6) # rolls dice 1
+    roll2 = random.randint(1,6) # rolls dice 2
     print(f"Player 2 rolled a {roll1} and a {roll2}")
     player_2total += roll1 + roll2
     if roll1 == roll2:
@@ -148,13 +148,13 @@ def s_lad():
       player_2total = snake4
       print("Player 2 got bitten by a snake to ",snake4)
 
-    if player_2total == 49: #checks if player 2 has won
-      print("Player 2 wins!")  #informs players that player 2 has won
-      print(f"player 1 was on {player_1total}") #informs players of player 1 position, if ur thinky why i dont show both player 2 and 1 position for this to run one player is at position 49 and the other player could be anywhere from 1 to 48 so i just show the other players position
-      print("Game Over") #informs players that the game is over
+    if player_2total == 49: # checks if player 2 has won
+      print("Player 2 wins!")  # informs players that player 2 has won
+      print(f"player 1 was on {player_1total}") # informs players of player 1 position, if ur thinky why i dont show both player 2 and 1 position for this to run one player is at position 49 and the other player could be anywhere from 1 to 48 so i just show the other players position
+      print("Game Over") # informs players that the game is over
       break 
     
-  return(player_1total, player_2total) #for the writing and whatever we need the results for idk , just returns the final positions of both players
+  return(player_1total, player_2total) # for the writing and whatever we need the results for idk , just returns the final positions of both players
 
 
 game = s_lad() #yk what this does
